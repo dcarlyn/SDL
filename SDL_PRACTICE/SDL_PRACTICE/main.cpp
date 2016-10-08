8bbd318e2cf2fecb05404c47b57e8de3b8a3c8eb
David Carlyn <davidcarlyn@Davids-MacBook-Pro-3.local>
2016-10-07 21:42:32 -0400

    Add files

---
+++ SDL_PRACTICE/SDL_PRACTICE/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  SDL_PRACTICE
+//
+//  Created by David Carlyn on 10/7/16.
+//  Copyright © 2016 CarlynGames. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::string str;
+    
+    std::cout << "What is your name?" << std::endl;
+    
+    std::cin >> str;
+    
+    std::cout << "Your name is " << str << std::endl;
+    
+    
+    
+    return 0;
+}
